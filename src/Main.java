56967ad68ecb2cb1a91cde86dd8fdf03ae2cdf51
Aleksandr Belazor <avbelazor@gmail.com>
2025-08-26 13:30:36 +0300

    Added message text

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Opening browser");
         System.out.println("Opening application URL");
         System.out.println("Logging is an Sasha/QWERTY1234");
-        System.out.println("Validating Welcome message appears");
+        System.out.println("Validating message 'Hello Alex' appears");
         }
     }
